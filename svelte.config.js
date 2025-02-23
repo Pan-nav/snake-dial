a08b7cc4d94db6fc51abb9c4dcb738602d71b409
Panav Bindal <102317251+Pan-nav@users.noreply.github.com>
2025-02-23 01:02:14 +0000

    please work

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
+		adapter: adapter()
 	}
 };
 
